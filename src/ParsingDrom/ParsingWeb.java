7e52d177fcd70b529908ba65c2ee4c3ff56355f7
Олеся Николаева <Niki9180@mail.ru>
2022-09-17 18:56:20 +0300

    adding comments

--- src/ParsingDrom/ParsingWeb.java
+++ src/ParsingDrom/ParsingWeb.java
@@ -1,3 +1,5 @@
+//Программа парсит сайт drom.ru и выводит результат в JTable
+
 package ParsingDrom;
 
 import java.io.IOException;
@@ -20,13 +22,13 @@ public class ParsingWeb {
         frame.setLocationRelativeTo(null); // выравниваем окно по центру
         Container container = frame.getContentPane();
         container.setLayout(new FlowLayout()); //последовательное расположение с выравниванием компонентов по центру
-        container.setBackground(Color.pink.brighter()); //устанавливаем цвет окна
+        container.setBackground(Color.pink.brighter());
         List<Auto> list = new ArrayList<>(); //создаем список для внесения данных
         Document doc = Jsoup.connect("https://auto.drom.ru/").get();//получаем html-код страницы
         Elements allElements = doc.getElementsByAttributeValue("data-ftid", "bulls-list_bull"); //получаем все элементы, у которых есть атрибут data-ftid со значением bulls-list_bull
             for (Element element : allElements) { //проходимся по всем элементам
                 String details = element.attr("href"); //у каждого элемента получаем значение атрибута "href" т.е. ссылку
-                Auto advert = new Auto(); //создаем объект класса Advert
+                Auto advert = new Auto();
                 advert.setLink(details);//устанавливаем для поля "link" значение - ссылку
                 Document detailsDoc = Jsoup.connect(details).get(); //подключаемся к ссылке для получения дополнительной информации
                 Elements el1 = detailsDoc.getElementsByTag("h1"); //из ссылки на объявление получаем тег h1
@@ -40,12 +42,12 @@ public class ParsingWeb {
         DefaultTableModel tableModel = new DefaultTableModel(); //создаем модель  по умолчанию, которая является таблицей нулевых столбцов и нулевых строк
         JTable table = new JTable(); //создаем объект JTable
         table.setModel(tableModel); //устанавливаем для JTable модель
-        table.setPreferredScrollableViewportSize(new Dimension(1100, 500)); //устанавливаем размер JTable
+        table.setPreferredScrollableViewportSize(new Dimension(1100, 500));
         table.setFillsViewportHeight(true); // чтобы таблица никогда не была меньше окна просмотра
         frame.add(new JScrollPane(table)); //JScrollPane обеспечивает представление с возможностью прокрутки компонент
-        Object[] column = new String[]{"Название", "Ссылка", "Цена", "Пробег"};
-        tableModel.setColumnIdentifiers(column);
-        for (int i = 0; i < list.size(); i++) {
+        Object[] column = new String[]{"Название", "Ссылка", "Цена", "Пробег"}; //заголовки для столбцов
+        tableModel.setColumnIdentifiers(column); //определение столбцов
+        for (int i = 0; i < list.size(); i++) { //заполнение модели данными
             String title = list.get(i).getTitle();
             String link = list.get(i).getLink();
             String price = list.get(i).getPrice();
